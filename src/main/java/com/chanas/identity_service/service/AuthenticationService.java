4548ff78574f2aae582ff1aff041841e22f6fcb2
baochan69a <lengocbaochan3009@gmail.com>
2026-04-08 15:53:16 +0700

    feat: implement JWT generation logic for user authentication
    Integrated Nimbus JOSE + JWT library to handle token creation.
    
    Implemented generateToken method using HS512 algorithm.
    
    Configured Payload with essential claims: sub (username), iat (issue time), exp (expiration set to 1 hour), and custom claims.
    
    Secured the token with a system-wide SIGNER_KEY.
    
    Handled checked exceptions (JOSEException) by wrapping them in RuntimeException for cleaner service logic.

--- src/main/java/com/chanas/identity_service/service/AuthenticationService.java
+++ src/main/java/com/chanas/identity_service/service/AuthenticationService.java
@@ -1,28 +1,81 @@
 package com.chanas.identity_service.service;
 
 import com.chanas.identity_service.dto.request.AuthenticationRequest;
+import com.chanas.identity_service.dto.response.AuthenticationResponse;
 import com.chanas.identity_service.exception.AppException;
 import com.chanas.identity_service.exception.ErrorCode;
 import com.chanas.identity_service.repository.UserRepository;
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password4j.BcryptPassword4jPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthenticationService {
     UserRepository userRepository;
 
-    public boolean authenticate(AuthenticationRequest request){
-        var user = userRepository.findByUsername(request.getUsername())
-                .orElseThrow(()-> new AppException(ErrorCode.USER_NOT_FOUND));
+    @NonFinal
+    protected static final String SIGNER_KEY = "494ebbdf7a1eac1877ebb5e9e75c22edc17aebd86a489f575da4dfc9190d156f";
 
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
-        return passwordEncoder.matches(request.getPassword(), user.getPassword());
+        var user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+
+        boolean authenticated = passwordEncoder
+                .matches(request.getPassword(), user.getPassword());
+
+        if (!authenticated) {
+            throw new AppException(ErrorCode.UNAUTHENTICATED);
+        }
+
+        var token = generateToken(request.getUsername());
+
+        return AuthenticationResponse.builder()
+                .token(token)
+                .authenticated(authenticated)
+                .build();
+
+    }
+
+    private String generateToken(String username){
+        JWSHeader header = new JWSHeader(JWSAlgorithm.HS512);
+
+        JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
+                .subject(username)
+                .issuer("chanas.com")
+                .issueTime(new Date())
+                .expirationTime(new Date(
+                        Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli()
+                ))
+//                .claim("Custom clain", "Custom")
+                .build();
+
+        Payload payload = new Payload(jwtClaimsSet.toJSONObject());
+
+        JWSObject jwsObject = new JWSObject(header, payload);
+
+        try {
+            jwsObject.sign(new MACSigner(SIGNER_KEY.getBytes()));
+            return jwsObject.serialize();
+        } catch (JOSEException e) {
+            log.error("Cannot create token, e");
+            throw new RuntimeException(e);
+        }
     }
 }
+
